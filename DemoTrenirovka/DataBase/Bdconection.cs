fce7cb1fa4ee9340e0a9d2b76bf7ecaa0ed90f8e
Егор Артемьев <batman_batman_1910@mail.ru>
2023-06-14 17:47:05 +0300

    First

---
+++ DemoTrenirovka/DataBase/Bdconection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTrenirovka.DataBase
+{
+    public class Bdconection
+    {
+        public static ToysEntities connection = new ToysEntities();
+    }
+}
